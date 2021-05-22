e0582de803c3a04149095ec1755e571abca59e8b
Kumar Saurabh Raj <kumarsaurabhraj.sr@gmail.com>
2021-05-22 07:12:15 +0530

    material init

--- src/Components/Landing/Banner.js
+++ src/Components/Landing/Banner.js
@@ -1,10 +1,58 @@
 import React from "react";
 import "./Banner.css";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translateX(-50%) translateY(-50%)"
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
 
 export default function Banner() {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+
     return (
-        <div className="landing-banner">
-            <h2>Saurabh</h2>
-        </div>
+        <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
     )
 }
---
+++ src/Content/landing.js
@@ -0,0 +1,17 @@
+const landingPageData = {
+    name: "Kumar Saurabh Raj",
+    subtitle1: "Full Stack Developer",
+    subtitle2: "Previously worked at",
+    previous: [
+        {
+            image: "/icons/GSoC.svg",
+            name: "Google Summer of Code"
+        },
+        {
+            image: "/icons/OMP.png",
+            name: "Open Mainframe Project"
+        }
+    ]
+}
+
+export default landingPageData;
